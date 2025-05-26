245594fa58ed4cae909981bbcd196a419c1cf0b2
yahiaough <yahiaough@gmail.com>
2025-05-26 16:15:08 +0200

    Add files via upload

---
+++ Exercice 2.1 - python.py
@@ -0,0 +1,12 @@
+#Exercice 2.1
+
+etudiant = {
+    "nom" : "Dupont",
+    "age" : "20",
+    "adresse" : "5 Rue du Guesclin"
+    }
+
+print(etudiant["nom"])
+print(etudiant["age"], "ans")
+print(etudiant["adresse"])
+    
