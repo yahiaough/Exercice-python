646e70c8944ac9b2b9fedebf3c18fbb425297118
yahiaough <yahiaough@gmail.com>
2025-05-27 10:35:19 +0200

    Add files via upload

--- Exercice 2.1 - python.py
+++ Exercice 2.1 - python.py
@@ -10,3 +10,39 @@ print(etudiant["nom"])
 print(etudiant["age"], "ans")
 print(etudiant["adresse"])
     
+    
+message = "Bonjour à vous tous"
+lettre = "o"
+
+message = message.lower()
+mots = message.split()
+occurence = {}
+
+for mot in mots:
+    occurence[mot] = mot.count(lettre)
+
+print(occurence)
+
+
+
+etudiant = {
+    "nom" : "Dupont",
+    "age" : "20",
+    "adresse" : "5 Rue du Guesclin"
+    }
+
+etudiant = {
+    "nom" :"Dupont",
+    "age" :"21",
+    "adresse" : "5 Rue du Guesclin"
+    }
+
+
+print(etudiant["nom"])
+print(etudiant["age"], "ans")
+print(etudiant["adresse"])
+
+
+
+
+
